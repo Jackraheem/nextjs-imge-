abe187f3bb822fa8811e1784f7839fed9d0f2d3e
jackraheem <rahimss109@gmail.com>
2024-12-17 23:05:49 +0530

    first update

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,35 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import hr from '../../public/ddp.webp';
+import "../app/globals.css"
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div style={{  alignItems: 'center', gap: '10px' }}>
+     <div style={{
+      backgroundImage: `url('https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649')`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+      height: '100vh' // Adjust height as needed
+    }}>
+     <h1 style={{display :"flex", justifyContent :"center" , alignItems:"center",height :"100vh"}}>HELLO  NEXT .JS WEBDADDY</h1>
+    </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <h1> Using the Image component</h1>
+      <p> Using Image component it is compress the image size</p>
+      <hr/>
+      <Image 
+        src={hr}
+        alt="Example description"
+        width={400} // Width in pixels
+        height={260} // Height in pixels
+      />
+      <h1>Normal img tag</h1><hr/>
+      <img src='my wall.jpg'  style={{height :"50%", width:"45%"}}/>
+      <h1>external source imge</h1>
+      <img src="https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649"  style={{height :"50%", width:"45%"}}/>
+
     </div>
   );
-}
+};
+
+export default HomePage;
